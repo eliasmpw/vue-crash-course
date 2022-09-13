a0b705e0f37c5f751028a07432b5829dff47d596
eliasmpw <eliasmpw@hotmail.com>
2022-09-13 15:23:41 -0400

    Task tracker code

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '/' },
+      },
+    },
+  },
+})
